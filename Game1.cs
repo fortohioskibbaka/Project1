11ce3814a127b8530cd44f227d44af28c8fbd01a
Parker Hammond <339720419@GoTVDSB.ca>
2024-11-25 08:58:51 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project1
+
+{
+    enum Screen
+    {
+        intro,
+        dinoback
+
+
+    }
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D introtexture;
+        Rectangle window;
+        SoundEffect soundd;
+
+        Screen Screen;
+
+        MouseState mouseState;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+
+
+            _graphics.ApplyChanges();
+
+            Screen = Screen.intro;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            introtexture = Content.Load<Texture2D>("dinoback");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (Screen == Screen.intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    Screen = Screen.dinoback;
+
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            if (Screen == Screen.intro)
+            {
+                _spriteBatch.Draw(introtexture, window, Color.White);
+
+
+            }
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
